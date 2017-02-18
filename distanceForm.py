84be606b2d774f8f6a722fee0ff10f94d1f4d35b
jrp562 <jrp562@msstate.edu>
2017-02-18 13:23:58 -0600

    V 0.02

--- distanceForm.py
+++ distanceForm.py
@@ -5,7 +5,7 @@ def distance(x1, x2, y1, y2):
 	dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 	return dist
 
-def main():
+def calcDistance():
 	x1 = int(input("Enter x1: "))
 	y1 = int(input("Enter y1: "))
 	x2 = int(input("Enter x2: "))
@@ -13,4 +13,4 @@ def main():
 
 	print("Distance between points: "distance(x1, x2, y1, y2))
 
-main()
+calcDistance()
