39c14e91a1bc17eedd2db318cf0217f92cf63d8c
Brandon Stone <bs1443@msstate.edu>
2017-02-24 13:19:11 -0600

    Add files via upload

--- TestBMICalc.py
+++ TestBMICalc.py
@@ -1,8 +1,22 @@
+#Author: Brandon Stone
 import pytest
-import sys
-from bmiCalculator import bodyMassIndex
 
-class BMI_Test:
-    def test_valid(self):
-        assert(bodyMass
+from bmiCalculator import *
+
+class TestBMI:
+
+    #Tests if the BMI Calculator works
+    def test_correct(self):
+        assert bodyMassIndex(5, 9, 167)==25.26
+
+    #Tests that it will not allow incorrect inputs
+    def test_incorrect(self):
+        assert bodyMassIndex(0,222,9)==0
+
+    #Last 2 Tests are for checking other vaules
+    def test_overweight(self):
+        assert bodyMassIndex(6,7,500)>=30
+
+    def test_underweight(self):
+        assert bodyMassIndex(5,7,100)<18
     
--- bmiCalculator.py
+++ bmiCalculator.py
@@ -1,32 +1,44 @@
-import math
+#Author: Brandon Stone
 
-class bodyMassIndex:
-    def __init__(self, feet, inches, weight):
-        while True:
-            try:
-                self.feet = feet * 12.0
-                self.inches = inches
-                self.weight = weight * .45
-            except ValueError:
-                print("Please enter valid inputs")
+#Calculates the BMI
+def bodyMassIndex(feet,inches,weight):
+    if feet<2 or weight<30 or weight>600 or inches<0:
+        return 0
+    else:
+        height = float(((feet*12) + inches) * .025)
+        bmi = (weight * .45) / (height ** 2)
+        return float(format(bmi, '.2f'))
+
+#Gets input from the user and decides their BMI category
+def index():
+    while True:
+        try:
+            print("Enter your Height in Ft and In and your Weight in Pounds.")
+            feet = float(input("Feet: "))
+            inches = float(input("Inches: "))
+            weight = float(input("Weight:  "))
+        except ValueError:
+            print("Please enter valid inputs integers.")
+            continue
+        else:
+            if feet < 2:
+                print("Please enter feet above 2.")
+                continue
+            elif inches < 0:
+                print("Please enter inches above 0.")
+            elif weight < 30 or weight > 600:
+                print("Enter weight that is between 30 and 600.")
                 continue
             else:
-                if feet <= 0 or inches <= 0 or weight <= 0:
-                    print("Please enter values that are above zero.")
-                    continue
-                else:
-                    break
+                break
 
-    def index(self):
-        meters = float((self.feet + self.inches) * 0.025)
-        meters = math.pow(meters, 2)
-        bmi = self.weight / meters
-        
-        if bmi <= 18.5:
-            return "BMI: " + "%.2f" % bmi + " Category: Underweight"
-        elif bmi >= 18.5 and bmi <= 24.9:
-            return "BMI: " + "%.2f" % bmi + " Category: Normal Weight"
-        elif bmi >= 25 and bmi <= 29.9:
-            return "BMI: " + "%.2f" % bmi + " Category: Overweight"
-        elif bmi >= 30:
-            return "BMI: " + "%.2f" % bmi + " Category: Obese"
+    bmi = bodyMassIndex(feet,inches,weight)
+    
+    if bmi < 18.5:
+        return "BMI: " + str(bmi) + " Category: Underweight"
+    elif bmi >= 18.5 and bmi <= 24.9:
+        return "BMI: " + str(bmi) + " Category: Normal Weight"
+    elif bmi >= 25 and bmi <= 29.9:
+        return "BMI: " + str(bmi) + " Category: Overweight"
+    elif bmi >= 30:
+        return "BMI: " + str(bmi) + " Category: Obese"
