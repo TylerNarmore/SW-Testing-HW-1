e79a013c5d0479c2a9bbeac3c332eb52d12b5ec5
TylerNarmore <tyler@narmore.net>
2017-02-26 21:33:05 -0600

    V0.40
    -Added Script to run Tests
    -Acutally fixed distance() arguments

---
+++ RunTest.bat
@@ -0,0 +1,1 @@
+start cmd /k pytest --cov=. TestBmiCalc.py TestDistance.py TestEmailVerifier.py TestMenu.py TestRetirement.py 
--- distanceForm.py
+++ distanceForm.py
@@ -1,7 +1,7 @@
 import sys
 import math
 
-def distance(x1, x2, y1, y2):
+def distance(x1, y1, x2, y2):
 	dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 	return "%.2f" % dist
 
